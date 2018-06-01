989a3652bb3d8374b7ee0368a1cfa36f14e4bd9e
trxrse12 <trxrse@gmail.com>
2018-06-01 16:21:29 +0100

    01/06/2018 H4. 1. the test_api.js is now evolved to compare what API returns to manual calculations

--- test/test_api.js
+++ test/test_api.js
@@ -8,7 +8,21 @@ var chai = require('chai'),
     expect = chai.expect;
 var superagent = require('superagent');
 
+const unitPrice = {
+    Soup: 0.65,
+    Bread: 0.80,
+    Milk: 1.15,
+    Apples: 1.00
+};
 
+const specialOffers = {
+    Milk: {discount_qty: 3,
+        discount_amt: 0.5}, // 50%
+    Apples: {discount_qty:1,
+        discount_amt: "10%"} // 10%
+};
+
+const currency = 2;
 
 context('\n\n\n\n\n\n\n\n ===>>>: TESTING THE Basket API:',function(){
         describe('\n\n CONTEXT 1: When hit by API request (GET /products), the Basket API: ', function() {
@@ -37,8 +51,15 @@ context('\n\n\n\n\n\n\n\n ===>>>: TESTING THE Basket API:',function(){
 
         describe('\n\n CONTEXT 3: When hit by API request (POST /basket), the Basket API: ', function(){
             it("TEST 1.3 (POST /basket) >>> should retrieve the cost of associated basket",function(done){
+                let apples = 3;
+                let milk = 4;
+                let soup = 1;
+
+                let itemsString = "Apples, ".repeat(apples) + "Milk, ".repeat(milk) + "Soup, ".repeat(soup);
+                itemsString = itemsString.slice(0,-1); // cut the last ","
+
                 superagent.post('http://localhost:3000/api/v1.0/basket').query({
-                        items:'Apples, Apples, Apples, Milk, Soup, Milk, Milk, Milk',
+                        items:itemsString,
                         currency: 'EUR'
                 })
                 .end(function(err,res){
@@ -53,8 +74,19 @@ context('\n\n\n\n\n\n\n\n ===>>>: TESTING THE Basket API:',function(){
                     res.body.should.have.a.property('subtotal');
                     res.body.subtotal.should.be.a('number');
                     res.body.subtotal.should.be.at.least(0);
-                    res.body.subtotal.should.be.closeTo(16.5,0.01); // excluding discount
-                    res.body.total.should.be.closeTo(14.9,0.01); // including discount
+
+                    let total_apples = 3 * unitPrice.Apples;
+                    let total_milk = 4 * unitPrice.Milk;
+                    let total_soup = 1 * unitPrice.Soup;
+                    let subtotal = (total_apples + total_milk + total_soup)*currency;
+
+                    let discount_apples = 3 * 0.1 * currency;
+                    let discount_milk = Math.floor(4  / specialOffers.Milk.discount_qty) * specialOffers.Milk.discount_amt * currency;
+                    let total_discount = (discount_apples + discount_milk);
+
+                    res.body.subtotal.should.be.closeTo(subtotal,0.01); // excluding discount
+                    res.body.discountAmt.should.be.closeTo(total_discount,0.01);
+                    res.body.total.should.be.closeTo(subtotal - total_discount,0.01); // including discount
 
                     res.body.should.have.a.property('discounts');
                     res.body.discounts.should.be.an('string');
